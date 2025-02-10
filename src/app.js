23218be5a09e8a998f7bdbbf6d2f56d01b46d168
Mridul-Desai <mridul.d2002@gmail.com>
2025-02-10 11:15:21 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 // Task 4 :-Create a for loop to print all the even numbers from 1 - 200 and return the same inside the function.You will need to create an empty array, create the logic and push the output to the array and return it inside the function.
 
 function loop(){
+    var arr[];
+    for(int i=1;i<201;i++){
+         if(i%2===0){
+            
+         }
+    }
 
 }
 // Task 5 :- create a variable of string datatype and return the string output inside the function given below
 
 function printStr(){
-  
+    let String1="String";
+    return String1;
+
 }
 
 //Task 6:- Using DOM methods, access the id 'task6' from index.html and return the text content of id 'task6' in the same function below.
 
 function domManip() {
-
+    let v=document.getElementById("task6").innerHTML;
+    return v;
 }
